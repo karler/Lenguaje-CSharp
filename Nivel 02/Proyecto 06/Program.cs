988bd79d5baf72d6cac695293d374c92d0621e9b
karler <karler01@gmail.com>
2024-04-17 12:09:21 -0500

    Proyecto 6 - Arrays y Foreach

---
+++ Nivel 02/Proyecto 06/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombres = {"Cristian","Yunior", "Cheila", "Jolauos", "Julio" };
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine($"Nombre {i} : {nombres[i]}");
+            }
+
+            foreach (string nombre in nombres)
+            {
+                Console.WriteLine($"Nombre : {nombre}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
