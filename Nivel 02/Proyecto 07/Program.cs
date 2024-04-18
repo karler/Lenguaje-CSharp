d0a1c4df05d0d8c7a3e163523efa93cc95de5750
karler <karler01@gmail.com>
2024-04-18 12:03:05 -0500

    Proyecto 07
    Juego, adivina la fruta que estoy pensando

---
+++ Nivel 02/Proyecto 07/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AdivinaLaPalabra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array de palabras para adivinar
+            string[] frutas = { "manzana", "banana", "cereza", "durazno", "fresa", "kiwi", "mango", "naranja", "pera", "piña" };
+            Random rnd = new Random();
+            int indice = rnd.Next(frutas.Length);
+            string frutaSecreta = frutas[indice];
+            Console.WriteLine("Adivina en cual de estas frutas estoy pensando");
+            foreach (string fruta in frutas)
+            {
+                Console.Write($"{fruta} ");
+            }
+            string frutaUsuario;
+            do
+            {
+                frutaUsuario = Console.ReadLine().ToLower();
+                if(frutaSecreta == frutaUsuario)
+                {
+                    Console.WriteLine("Felicidades, adivinaste mi fruta secreta !! :D");
+                }
+                else
+                {
+                    Console.WriteLine("Intenta de nuevo...");
+                }
+            }while(frutaSecreta != frutaUsuario);
+            Console.ReadLine();
+        }
+    }
+}
