978813051a5cb69fac9370deafa932f6b5ae106a
karler <karler01@gmail.com>
2024-04-25 13:13:02 -0500

    Terminamos funciones

---
+++ Nivel 03/Proyecto 08/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Argumentos - Paso por salida
+            int mul;
+            double div;
+            int res;
+            int sum;
+            void operaciones(int a, int b, out int s, out int m, out double d, out int r)
+            {
+                r = a - b;
+                m = a * b;
+                d = a / b;
+                s = a + b;
+            }
+            // Parámetros nombrados a: 4, b: 3
+            operaciones(a: 4, b: 3, out sum, out mul, out div, out res);
+            Console.WriteLine($"suma = {sum} - Resta = {res} - Muti = {mul} - Div = {div}");
+            Console.ReadLine();
+        }
+    }
+}
