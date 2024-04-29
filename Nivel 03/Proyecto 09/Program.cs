f8ef2c38b6cf797902e2aa6bbefb58e9f8ad5173
karler <karler01@gmail.com>
2024-04-29 08:59:47 -0500

    Ambito de variables

---
+++ Nivel 03/Proyecto 09/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_09
+{
+    internal class Program
+    {
+        public int z;
+        static void Main(string[] args)
+        {
+            int y = 10;
+            if (y >= 5)
+            {
+                int x = 10; // x solo es accesible dentro del bloque if
+                y = 2;
+            }else
+            {
+                y = 20;
+
+            }
+            // x no es accesible aquí
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
