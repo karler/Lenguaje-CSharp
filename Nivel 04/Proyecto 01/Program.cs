f349bd01f578ee9ed153310f4def79b873604b72
karler <karler01@gmail.com>
2024-05-02 11:54:26 -0500

    Proyecto 4.1

--- Nivel 04/Proyecto 01/Program.cs
+++ Nivel 04/Proyecto 01/Program.cs
@@ -1,16 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Proyecto_01
 {
     internal class Program
     {
+        public class Perro
+        {
+            // Propiedades
+            public string Nombre { get; set; }
+            public string Raza { get; set; }
+            public int Edad { get; set; }
+
+            // Método
+            public void Ladrar()
+            {
+                Console.WriteLine($"{Nombre}: Guau!");
+            }
+        }
         static void Main(string[] args)
         {
+            // Creación de objetos
+            Perro miPerro = new Perro();
+            miPerro.Nombre = "Buddy";
+            miPerro.Raza = "Labrador";
+            miPerro.Edad = 5;
 
+            Perro perroVecino = new Perro();
+            perroVecino.Nombre = "Rex";
+            perroVecino.Raza = "Pastor Alemán";
+            perroVecino.Edad = 3;
+
+            // Uso de métodos
+            miPerro.Ladrar(); // Salida: Guau!
+            perroVecino.Ladrar();
+            Console.ReadLine();
         }
     }
 }
