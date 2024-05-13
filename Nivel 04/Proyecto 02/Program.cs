dd0e0624a59146c104854d522f618f2a54a1dd5e
karler <karler01@gmail.com>
2024-05-13 09:11:22 -0500

    2 operaciones
    Agregamos la resta al proyecto 2

--- Nivel 04/Proyecto 02/Program.cs
+++ Nivel 04/Proyecto 02/Program.cs
@@ -4,24 +4,60 @@ namespace Proyecto_02
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             Random aleatorio = new Random();
 
-            int num1 = aleatorio.Next(10,101);
-            int num2 = aleatorio.Next(10, 101);
+            int num1;
+            int num2;
+            int operacion;
+            int puntaje = 0;
             int respuesta;
-            int resultado = num1 + num2;
-            Console.Write($"{num1} + {num2} = ");
-            respuesta = Convert.ToInt32(Console.ReadLine());
-            if(respuesta == resultado ) {
-                Console.WriteLine("¡Correcto!");
-            } else
+            int resultado=0;
+            bool juego = true;
+
+            while (juego)
             {
-                Console.WriteLine("Perdiste!");
+                //Generamos los numeros aleatorios
+                num1 = aleatorio.Next(1, 15);
+                num2 = aleatorio.Next(5, 20);
+                operacion = aleatorio.Next(1, 3);
+               
+                //Mostrar en pantalla la operación
+                if(operacion == 1)
+                {
+                    Console.Write($"{num1} + {num2} = ");
+                }
+                if(operacion == 2)
+                {
+                    Console.Write($"{num1} - {num2} = ");
+                }
+                
+                // Calculamos el resultado
+                if (operacion == 1)
+                {
+                    resultado = num1 + num2;
+                }
+                if (operacion == 2)
+                {
+                    resultado = num1 - num2;
+                }
+                
+                //Recibimos la respuesta del usuario
+                respuesta = Convert.ToInt32(Console.ReadLine());
+
+                //comparamos las respuestas
+                if (respuesta == resultado)
+                {
+                    Console.WriteLine("¡Correcto!");
+                    puntaje++;
+                }
+                else
+                {
+                    Console.WriteLine($"Perdiste!  tu puntaje es: {puntaje}");
+                    juego = false;
+                }
             }
             Console.ReadLine();
-
         }
     }
 }
---
+++ Nivel 04/Proyecto 03/Program.cs
@@ -0,0 +1,71 @@
+﻿using Proyecto_02;
+using System;
+
+namespace Proyecto_02
+{
+    // Clase para manejar las operaciones de suma
+    public class OperacionSuma
+    {
+        public int Numero1 { get; private set; }
+        public int Numero2 { get; private set; }
+        private Random random = new Random();
+
+        public OperacionSuma()
+        {
+            // Al crear una nueva operación, generamos dos números aleatorios
+            Numero1 = random.Next(1, 101);
+            Numero2 = random.Next(1, 101);
+        }
+
+        public int CalcularResultado()
+        {
+            return Numero1 + Numero2;
+        }
+
+        public string pregunta()
+        {
+            return $"{Numero1} + {Numero2} = ";
+        }
+    }
+
+    // Clase para manejar la lógica del juego
+    public class JuegoSuma
+    {
+        private int puntaje = 0;
+        private bool juegoActivo = true;
+
+        public void Jugar()
+        {
+            while (juegoActivo)
+            {
+                OperacionSuma operacion = new OperacionSuma();
+                Console.Write(operacion.pregunta());
+
+                int respuestaUsuario = Convert.ToInt32(Console.ReadLine());
+                int resultadoCorrecto = operacion.CalcularResultado();
+
+                if (respuestaUsuario == resultadoCorrecto)
+                {
+                    puntaje++;
+                    Console.WriteLine("¡Correcto!");
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrecto. Fin del juego. Puntaje = {puntaje}");
+                    juegoActivo = false;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+    // Clase principal que inicia el juego
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            JuegoSuma juego = new JuegoSuma();
+            juego.Jugar();
+        }
+    }
+}
