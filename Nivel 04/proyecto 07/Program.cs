997b6219b34cc3776ce3184888d26ae5b5f8751e
karler <karler01@gmail.com>
2024-06-03 10:59:27 -0500

    Calculadora POO

---
+++ Nivel 04/proyecto 07/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace proyecto_07
+{
+    internal class Program
+    {
+        public class Cesar
+        {
+            public string Cifrar(string texto)
+            {
+                char[] alfabeto = "abcdefghijklmnopqrstuvwxyz ñ".ToCharArray();
+                char[] textoOriginal = texto.ToCharArray();
+                string textoCifrado = "";
+                int i;
+                int pos;
+                int desplazamiento = 5;
+                // de 0 hasta el final
+                for (i = 0; i < textoOriginal.Length; i++)
+                {
+                    pos = Array.IndexOf(alfabeto, textoOriginal[i]);
+                    if (pos + desplazamiento > alfabeto.Length-1)
+                    {
+                        pos = pos - alfabeto.Length;
+                    }
+                    textoCifrado = textoCifrado + alfabeto[pos + desplazamiento];
+                }
+                return textoCifrado;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string texto;
+            Cesar cifradoCesar = new Cesar();
+            texto = Console.ReadLine();
+            Console.WriteLine(cifradoCesar.Cifrar(texto));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Nivel 04/proyecto 08/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace proyecto_08
+{
+    public class calculadora
+    {
+        private double a;
+        private double b;
+        private string operacion;
+        private double resultado;
+
+        public void ingresarDatos()
+        {
+            Console.Write("Ingrese el 1er número: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese la operación (+, -, *, /): ");
+            operacion = Console.ReadLine();
+            Console.Write("Ingrese el 2do número: ");
+            b = double.Parse(Console.ReadLine());
+        }
+        private double sumar()
+        {
+            return a + b;
+        }
+        private double restar()
+        {
+            return a - b;
+        }
+        private double multiplicar()
+        {
+            return b * a;
+        }
+        private double dividir()
+        {
+            if (b == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+        public string MostrarResultado()
+        {
+            return $"El resultado es: {resultado}";
+        }
+        public void calcular() 
+        {
+            switch (operacion)
+            {
+                case "+":
+                    resultado = sumar();
+                    break;
+                case "-":
+                    resultado = restar();
+                    break;
+                case "*":
+                    resultado = multiplicar();
+                    break;
+                case "/":
+                    resultado = dividir();
+                    break;
+                default:
+                    Console.WriteLine("Operación erronea");
+                    break;
+            }
+        }
+    }
+        
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculadora micalculadora = new calculadora();
+            micalculadora.ingresarDatos();
+            micalculadora.calcular();
+            Console.WriteLine(micalculadora.MostrarResultado());
+            Console.ReadLine();
+        }
+    }
+}
